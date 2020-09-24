4086787cb0b0888ef710687bec3db66dca9f3740
kakuzops <kakuzops@gmail.com>
2020-09-24 14:17:03 -0300

    pragmatic programming live view

---
+++ lib/live_view_studio_web/live/license_live.ex
@@ -0,0 +1,52 @@
+defmodule LiveViewStudioWeb.LicenseLive do
+  use LiveViewStudioWeb, :live_view
+
+  alias LiveViewStudio.Licenses
+  import Number.Currency
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, seats: 2, amount: Licenses.calculate(2))
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+      <h1>License team</h1>
+      <div id="license">
+        <div class="card">
+          <div class="content">
+            <div class="seats">
+              <img src="images/license.svg">
+
+              <span>
+                your license is currently for
+                <strong><%= @seats %></strong> seats
+              </span>
+            </div>
+
+            <form phx-change="update">
+              <input type="range" min="1" max="10"
+              name="seats" vaule="<%= @seats %>" />
+            </form>
+
+            <div class="amount">
+                <%= number_to_currency(@amount) %>
+            </div>
+          </div>
+        </div>
+      </div>
+    """
+  end
+
+  def handle_event("update", %{"seats" => seats}, socket) do
+    seats = String.to_integer(seats)
+
+    socket =
+      assign(socket,
+        seats: seats,
+        amount: Licenses.calculate(seats)
+      )
+
+    {:noreply, socket}
+  end
+end
